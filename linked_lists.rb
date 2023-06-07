f72547b6f64601e43786e615cf539fbfb02d36c8
Vitor Cardoso <vitorcnf64@gmail.com>
2023-06-07 16:42:42 -0300

    finally fixed #append, next_node is not ALWAYS nil anymore, because of this #size now works

--- linked_lists.rb
+++ linked_lists.rb
@@ -6,15 +6,21 @@ class LinkedList
   end
 
   # Adds a node to the end of the list.
+  # - Checks if @head is nil, adds a node there if it is.
+  # - Checks if @tail is nil, it means there's only one node. Creates new
+  # node and assigns it to @tail. Updates @head's next_node to @tail.
+  # - If both cases are false, there's >1 node. Creates new node and sets it as
+  # the current @tail's next_node, then assigns the new node to @tail.
   def append(data)
     if @head.nil?
       prepend(data)
+    elsif @tail.nil?
+      @tail = Node.new(data, nil)
+      @head.next_node = @tail
     else
-      tmp = Node.new
-      tmp = tmp.next_node until tmp.next_node.nil?
-
-      tmp.next_node = Node.new(data, nil)
-      @tail = tmp.next_node
+      new_node = Node.new(data, nil)
+      @tail.next_node = new_node
+      @tail = new_node
     end
   end
 
@@ -25,10 +31,10 @@ class LinkedList
 
   # Returns linked list size.
   def size
-    tmp = @head
+    current = @head
     size = 0
-    until tmp.nil?
-      tmp = tmp.next_node
+    until current.nil?
+      current = current.next_node
       size += 1
     end
     size
